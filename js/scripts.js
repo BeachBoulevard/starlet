0472ed1e2c55219429c7f8b878d6d08a8e41a662
Carl Tayo <tayocarl@gmail.com>
2018-06-14 22:31:34 +0300

    randomiser function is running

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,19 @@ $(document).ready(function(){
   $('button').delay(1100).hide().show(1000);
   $('#rowa').delay(2100).hide(500);
 
-  $.fn.random = function() {
-  return this.eq(Math.floor(Math.random() * this.length));
+
+$("button").click(function(){
+  alert("All you will be doing here is clicking");
+
+})
+
+$.fn.random = function() {
+return this.eq(Math.floor(Math.random() * this.length));
 }
 
-$(selector).random();
+$("button").random().show();
+
+
+
 
 });
