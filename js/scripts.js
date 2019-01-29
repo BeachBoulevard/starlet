9dcaee56fe2e82a2ee7892ffc18e20beb6ece637
BeachBoulevard <tayocarl@gmail.com>
2019-01-29 13:38:16 +0100

    Randomization and display of random element fn, need to ensure button maintains position

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 
   $('button').delay(1100).hide().show(1000);
-  $('#rowa').delay(2100).hide(500);
+  $('.flexContainer2').delay(2100).hide(500);
 
 
 $("button").click(function(){
-  var random = Math.floor(Math.random() * $('#rowa').length);
-    $('#rowa').eq(random).show(1000);  
+  var random = Math.floor(Math.random() * $('.flexContainer2').length);
+    $('.flexContainer2').eq(random).show(1000).hide(1000);  
 
 })
 
